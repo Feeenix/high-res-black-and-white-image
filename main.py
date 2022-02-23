697f1b93ded46421dbf2820c70aa871c17c709b1
Feeenix <feeeeeeenix@gmail.com>
2022-02-23 21:56:45 +0100

    cleanup

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from config import *
 
 def main():
 
-    pass
     install_package("numpy","opencv-python")
     sys.path.append("libraries")
     import cv2
@@ -18,17 +17,11 @@ def main():
         img_file_ = sys.argv[1]
         if len(sys.argv) > 2:
             out_file_ = sys.argv[2]
-    # print("⠀")
-    # print((ord("⠀")+0))
-    # print("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
-    # print ("".join([chr(i+10240) for i in range(256)]))
 
     if not os.path.isdir(in_dir): os.mkdir(in_dir)
     if not os.path.isdir(out_dir): os.mkdir(out_dir)
 
     bw_im = np.sum(cv2.imread(img_file_),axis=2)>threshold
-    # print(bw_im)
-
 
     vt_group = np.array([bw_im[i*4:i*4+4] for i in range(math.ceil(len(bw_im)/4))])
 
@@ -36,7 +29,7 @@ def main():
     chr(10240+ a[0][b*2+0]*1 + a[1][b*2+0]*2 + a[2][b*2+0]*4 + a[0][b*2+1]*8 + a[1][b*2+1]*16 + a[2][b*2+1]*32 + a[3][b*2+0]*64 + a[3][b*2+1]*128 )
     for b in range(math.ceil(len(a[0])/2))]
     for a in vt_group ])
-    # print (hz_group)
+    
     text = "\n".join(["".join(a) for a in hz_group])
     with open(out_file_, "w",encoding="utf-16") as f:
         f.write(text)
