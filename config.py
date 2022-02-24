d5b446de187a62ed5a70450dea110032569a39b1
Feeenix <feeeeeeenix@gmail.com>
2022-02-24 13:31:43 +0100

    asda

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 in_dir = "in"
 out_dir = "out"
 
-img_file = in_dir+"/Screenshot 2022-02-23 220736.png"
+img_file = in_dir+"/Screenshot 2022-02-24 014120.png"
 out_file = out_dir+"/out.txt"
-threshold = 200
+threshold = 252
 
 
 
